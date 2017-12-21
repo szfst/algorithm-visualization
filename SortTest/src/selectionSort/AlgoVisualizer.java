e7bd9ef9392888005da7e7aaf203e8331d9cba2b
fangshentian <jfdkajl>
2017-12-21 11:37:50 +0800

    initalization

---
+++ SortTest/src/selectionSort/AlgoVisualizer.java
@@ -0,0 +1,64 @@
+package selectionSort;
+
+import java.awt.*;
+
+public class AlgoVisualizer {
+
+    private static int DELAY = 20;
+
+    private SelectionSortData data;
+    private AlgoFrame frame;
+
+    public AlgoVisualizer(int sceneWidth, int sceneHeight, int N){
+
+        data = new SelectionSortData(N, sceneHeight);
+
+        // 初始化视图
+        EventQueue.invokeLater(() -> {
+            frame = new AlgoFrame("Selection Sort Visualization", sceneWidth, sceneHeight);
+            new Thread(() -> {
+                run();
+             }).start();
+        });
+    }
+
+    private void run(){
+        setData(0,-1,-1);
+        for(int i = 0;i<data.N();i++){
+            int temp = i;
+            setData(0,-1,temp);
+            for(int j = i+1;j<data.N();j++){
+                setData(i,j,temp);
+                if(data.get(j)<data.get(temp)){
+                    temp = j;
+                    setData(i,j,temp);
+                }
+
+            }
+            data.swap(i,temp);
+            setData(i,-1,-1);
+        }
+        setData(0,-1,-1);
+
+
+
+    }
+
+    private void setData(int orderedIndex, int currentCompareIndex, int currentMinIndex){
+        data.orderedIndex = orderedIndex;
+        data.currentCompareIndex = currentCompareIndex;
+        data.currentMinIndex = currentMinIndex;
+
+        frame.render(data);
+        AlgoVisHelper.pause(DELAY);
+    }
+
+    public static void main(String[] args) {
+
+        int sceneWidth = 800;
+        int sceneHeight = 800;
+        int N = 100;
+
+        AlgoVisualizer vis = new AlgoVisualizer(sceneWidth, sceneHeight, N);
+    }
+}
